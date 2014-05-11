4c04cd71edea9cfbd401c0d17290ca6a2f975a1a
spencereldred <eldredspencer@gmail.com>
2014-05-11 07:24:05 -0700

    add adventure controller tests

--- spec/controllers/adventures_controller_spec.rb
+++ spec/controllers/adventures_controller_spec.rb
@@ -1,13 +1,62 @@
-# require 'spec_helper'
+require 'spec_helper'
 
-# describe AdventuresController do
+describe AdventuresController do
 
-  # describe "GET 'index'" do
-  #   it "returns http success" do
-  #     get 'index'
-  #     response.should be_success
-  #   end
-  # end
+  render_views
+
+  describe "GET 'index'" do
+    it "returns http success for html" do
+      get 'index'
+      response.should be_success
+    end
+    before do
+      @adventures = []
+      for i in 1..5 do
+        @adventures << Adventure.create!(:title => "#{i} Test Adventure", :author => "person #{i}")
+      end
+    end
+    it "displays a list of adventures for html" do 
+      get 'index'
+      response.body.should include @adventures[1].title
+      response.body.should include @adventures[3].author
+    end
+    it "returns http success for json" do 
+      get 'index', { format: :json }
+      response.should be_success
+    end
+    before do
+       @adventures = []
+       for i in 1..5 do
+         @adventures << Adventure.create!(:title => "#{i} Test Adventure", :author => "person #{i}")
+       end
+     end
+     it "returns a list of adventures for json" do 
+       get 'index', {format: :json}
+       response.body.should include @adventures[1].title.to_json
+       response.body.should include @adventures[3].author.to_json
+     end
+   end
+ 
+   describe "GET 'show'" do
+     before do
+       i = 1
+       @adventure = Adventure.create!(:title => "#{i} show Test Adventure", :author => " show person #{i}")
+     end
+     it "returns http success for html" do
+       get 'show', {id: @adventure.id}
+       response.should be_success
+     end
+     before do
+       i = 2
+       @adventure = Adventure.create!(:title => "#{i} show Test Adventure", :author => " show person #{i}")
+     end
+     it "includes the adventure" do
+       get 'show', {id: @adventure.id}
+       response.body.should include(@adventure.title)
+     end
+   end
+  
+  end
 
   # describe "GET 'show'" do
   #   it "returns http success" do
--- spec/controllers/libraries_controller_spec.rb
+++ spec/controllers/libraries_controller_spec.rb
@@ -2,12 +2,32 @@ require 'spec_helper'
 
 describe LibrariesController do
 
-  # describe "GET 'index'" do
-  #   it "returns http success" do
-  #     get 'index'
-  #     response.should be_success
-  #   end
-  # end
+  describe "GET 'index'" do
+    before do
+      @libraries = []
+      for i in 1..5 do 
+        @libraries << Library.create(url: "google.com#{i}")
+      end
+    end
+    it "returns http success for html" do
+      get 'index'
+      response.status.should == 200
+    end
+    it "includes libraries" do 
+      get 'index'
+      response.body.should include(@libraries[0].url)
+      response.body.should include(@libraries[3].url)
+    end
+    it "returns http success for json" do
+      get 'index', { format: :json }
+      response.status.should == 200
+    end
+    it "includes libraries for json" do 
+      get 'index', { format: :json }
+      response.body.should include @libraries[3].to_json
+      response.body.should include @libraries[1].to_json
+    end
+  end
 
   render_views
 
@@ -16,10 +36,10 @@ describe LibrariesController do
       @library = Library.create(url: "google.com")  
     end
     it "returns http success for html" do
-      get 'show', { id: @library.id}
-      response.should be_success
+      get 'show', { id: @library.id, format: :json}
+      response.status.should == 200
     end
-    it "includes the library" do 
+    it "includes the library for json" do 
       get "show", { id: @library.id }
       response.body.should include @library.url.to_s
     end
@@ -33,11 +53,15 @@ describe LibrariesController do
     end
   end
 
-  # describe "GET 'new'" do
-  #   it "returns http success" do
-  #     get 'new'
-  #     response.should be_success
-  #   end
-  # end
+  describe "GET 'new'" do
+    it "returns http success for html" do
+      get 'new'
+      response.status.should == 200
+    end
+    it "returns 404 for json" do 
+      get 'new', { format: :json }
+      response.status.should == 404
+    end
+  end
 
 end
