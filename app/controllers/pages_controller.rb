986b0165af43cd0801803283eeb5eb009ca2d7a5
spencereldred <eldredspencer@gmail.com>
2014-05-11 08:15:01 -0700

    Adventure flow works

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,7 @@ class PagesController < ApplicationController
   end
 
   def show
+    @page = Page.find(id)
   end
 
   def edit
@@ -10,4 +11,10 @@ class PagesController < ApplicationController
 
   def new
   end
+
+  private
+
+    def id
+      params[:id]
+    end
 end
